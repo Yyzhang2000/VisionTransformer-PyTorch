977d47130543fdd695ec8713ffcf1ed11084b504
YuyangZhang <yuyangzhang2025@gmail.com>
2025-04-18 19:09:35 +0800

    Add training code

---
+++ main.py
@@ -0,0 +1,118 @@
+import torch
+import torch.nn as nn
+import yaml
+
+import torch.optim as optim
+import torch.nn.functional as F
+
+import logging
+from torch.utils.tensorboard import SummaryWriter  # type: ignore
+
+from model.vit import ViT
+from model.config import ViTConfig
+
+from torch.utils.data import DataLoader
+from dataset import FruitDataset
+
+
+from utils import *
+from train_engine import train
+
+
+###### CONFIGURATION ######
+DATA_DIR = "./data/MY_data"
+EXP_DIR = "./logs"
+
+TRAINING_CONFIG = {
+    "experiment_name": "vit_fruit_classification",
+    "seed": 42,
+    "batch_size": 128,
+    "epochs": 40,
+    "learning_rate": 0.001,
+    "lr_scheduler_step": 10,
+    "lr_scheduler_gamma": 0.1,
+}
+MODEL_CONFIG = {
+    "image_size": 224,
+    "patch_size": 16,
+    "num_classes": 10,  # Number of classes in the dataset
+    "hidden_states": 512,
+    "num_layers": 8,
+    "dropout": 0.1,
+}
+###### CONFIGURATION ######
+
+
+if __name__ == "__main__":
+    model_dir = os.path.join(EXP_DIR, TRAINING_CONFIG["experiment_name"])
+    if not os.path.exists(model_dir):
+        os.makedirs(model_dir)
+
+    set_seed(TRAINING_CONFIG["seed"])
+
+    set_logger(os.path.join(model_dir, "train.log"))
+    logging.getLogger()
+
+    # device =
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    logging.info(f"Using device: {device}")
+
+    ### Load dataset
+    train_dataset = FruitDataset(DATA_DIR, split="train")
+    test_dataset = FruitDataset(DATA_DIR, split="test")
+    train_loader = DataLoader(
+        train_dataset, batch_size=TRAINING_CONFIG["batch_size"], shuffle=True
+    )
+    test_loader = DataLoader(
+        test_dataset, batch_size=TRAINING_CONFIG["batch_size"], shuffle=False
+    )
+
+    ### Load and Save Config
+    MODEL_CONFIG.update(num_classes=len(train_dataset.classes))
+    model_config = ViTConfig(**MODEL_CONFIG)
+    config_dict = {
+        "model_config": config_to_dict(model_config),
+        "training_config": TRAINING_CONFIG,
+    }
+
+    with open(
+        os.path.join(EXP_DIR, TRAINING_CONFIG["experiment_name"], "config.yaml"), "w"
+    ) as f:
+        yaml.dump(config_dict, f)
+    logging.info(f"Configuration saved to {os.path.join(model_dir, 'config.yaml')}")
+
+    writer = SummaryWriter(model_dir)
+
+    # Initialize Model
+    model = ViT(model_config)
+
+    ## Optimizer and Learning Rate Scheduler
+    optimizer = optim.Adam(model.parameters(), lr=TRAINING_CONFIG["learning_rate"])
+    scheduler = optim.lr_scheduler.StepLR(
+        optimizer,
+        step_size=TRAINING_CONFIG["lr_scheduler_step"],
+        gamma=TRAINING_CONFIG["lr_scheduler_gamma"],
+    )
+    criterion = nn.CrossEntropyLoss()
+
+    train(
+        model=model,
+        train_loader=train_loader,
+        test_loader=test_loader,
+        optimizer=optimizer,
+        criterion=criterion,
+        num_epochs=20,
+        device=device,
+        scheduler=scheduler,
+        writer=writer,
+    )
+
+    torch.save(
+        model.state_dict(),
+        os.path.join(model_dir, "model.pth"),
+    )
+    logging.info(f"Final model saved to {os.path.join(model_dir, "model.pth")}")
+    writer.close()
+    logging.info("Training completed.")
+    logging.info("TensorBoard logs saved.")
+    logging.info("Model saved.")
---
+++ train_engine.py
@@ -0,0 +1,129 @@
+import torch
+import torch.nn.functional as F
+from tqdm import tqdm
+import os
+
+import logging
+
+import shutil
+
+
+def train_one_epoch(
+    model, train_loader, optimizer, criterion, device, epoch, writer=None
+):
+    model.train()
+    running_loss = 0.0
+    correct = 0
+    total = 0
+
+    progress_bar = tqdm(
+        enumerate(train_loader), total=len(train_loader), desc=f"Epoch {epoch+1}"
+    )
+    for batch_idx, (images, labels) in progress_bar:
+        images, labels = images.to(device), labels.to(device)
+
+        outputs = model(images)
+
+        loss = criterion(outputs, labels)
+
+        if writer:
+            step = epoch * len(train_loader) + batch_idx
+            writer.add_scalar("Loss/train", loss.item(), step)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item() * images.size(0)
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+        progress_bar.set_postfix(loss=loss.item(), accuracy=100 * correct / total)
+        if writer and epoch is not None:
+            step = epoch * len(train_loader) + batch_idx
+            writer.add_scalar("Loss/train", loss.item(), step)
+            writer.add_scalar("Accuracy/train", 100 * correct / total, step)
+
+    epoch_loss = running_loss / len(train_loader.dataset)
+    epoch_acc = 100 * correct / total
+
+    return epoch_loss, epoch_acc
+
+
+def test(model, test_loader, criterion, device, writer=None, epoch=None):
+    model.eval()
+    running_loss = 0.0
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        progress_bar = tqdm(
+            enumerate(test_loader),
+            total=len(test_loader),
+            desc=f"Test Epoch {epoch+1}" if epoch is not None else "Evaluating",
+        )
+        for batch_idx, (images, labels) in progress_bar:
+            images, labels = images.to(device), labels.to(device)
+
+            outputs = model(images)
+            loss = criterion(outputs, labels)
+
+            running_loss += loss.item() * images.size(0)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+            progress_bar.set_postfix(loss=loss.item(), accuracy=100 * correct / total)
+            if writer and epoch is not None:
+                step = epoch * len(test_loader) + batch_idx
+                writer.add_scalar("Loss/test", loss.item(), step)
+                writer.add_scalar("Accuracy/test", 100 * correct / total, step)
+
+    epoch_loss = running_loss / len(test_loader.dataset)
+    epoch_acc = 100 * correct / total
+    return epoch_loss, epoch_acc
+
+
+def train(
+    model,
+    train_loader,
+    test_loader,
+    optimizer,
+    criterion,
+    device,
+    num_epochs=20,
+    scheduler=None,
+    model_dir: str = "./logs/model",
+    writer=None,
+):
+    best_acc = 0.0
+
+    for epoch in range(num_epochs):
+        train_loss, train_acc = train_one_epoch(
+            model, train_loader, optimizer, criterion, device, epoch, writer
+        )
+        test_loss, test_acc = test(model, test_loader, criterion, device)
+
+        logging.info(
+            f"Epoch [{epoch + 1}/{num_epochs}], "
+            f"Train Loss: {train_loss:.4f}, Train Acc: {train_acc:.2f}%, "
+            f"Test Loss: {test_loss:.4f}, Test Acc: {test_acc:.2f}%"
+        )
+
+        if scheduler:
+            scheduler.step()
+
+        torch.save(
+            model.state_dict(),
+            os.path.join(model_dir, f"latest_model.pth"),
+        )
+        if test_acc > best_acc:
+            best_acc = test_acc
+            shutil.copyfile(
+                os.path.join(model_dir, "latest_model.pth"),
+                os.path.join(model_dir, "best_model.pth"),
+            )
+
+            logging.info(f"Best model saved with accuracy: {best_acc:.2f}%")
+
+    logging.info(f"Training complete. Best test accuracy: {best_acc:.2f}%")
