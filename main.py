6a051ab01993ebd05524a62e32d9f67eb8833aaf
YuyangZhang <yuyangzhang2025@gmail.com>
2025-04-18 19:17:19 +0800

    fix bug

--- main.py
+++ main.py
@@ -54,7 +54,8 @@ if __name__ == "__main__":
     logging.getLogger()
 
     # device =
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    device = get_device()
     logging.info(f"Using device: {device}")
 
     ### Load dataset
